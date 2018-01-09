872f0d39bbeee36493b362dc5e0a90cbdfed5e51
Melissa Real <melissa.real@hotmail.com>
2018-01-09 16:02:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function happyHolidayTo(holiday, name){
 
 function holidayCountdown(days, holiday){
  
- return( `Its\'s ${days} days until ${holiday}!`);
+ return( `Its\'s ${holiday} days until ${days}!`);
  
 }
